e91850c26f464dffde46ede14cbf6cd8dbf8ced3
Kristoffer <kristoffer.ln@gmail.com>
2018-07-26 19:53:57 +0200

    Fix css

--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 import VueGoodTable from 'vue-good-table';
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'vue-good-table/dist/vue-good-table.css';
 
 Vue.config.productionTip = false;
-
+Vue.use(BootstrapVue);
 Vue.use(VueGoodTable);
 /* eslint-disable no-new */
 new Vue({
